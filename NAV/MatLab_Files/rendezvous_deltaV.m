da8c13f5fef8250739dd6e0c4e13e8a14e6ad92e
tomamoline2 <tmoline@slu.edu>
2013-10-11 16:38:07 -0500

    Made for loop, no longer works, vertcat error for M

--- NAV/MatLab_Files/rendezvous_deltaV.m
+++ NAV/MatLab_Files/rendezvous_deltaV.m
@@ -16,7 +16,7 @@
 % Ra = Orbital Apoapsis Altitude (km)
 % rstar_ =Inital Location of Chase Satellite Relative to Focal Point(km)
 % r_ = Initial Location of Target Satellite Relatvie to Focal Point (km)
-%    NOTE: This is assumed to be a 2-D Vector input [x, y]
+%    NOTE: This is assumed to be a 3-D Vector input [x, y]
 %
 % All variables with star in name are of chase satellite. All others are of
 % the target satellite.
@@ -42,23 +42,38 @@ period=2*pi*sqrt(a^3/mu);
 rstar_ = input('Please input the inital displacement vector of chase satellite: ');
 r_ = input('Please input the initial displacement vector of target satellite: ');
 
+% Calculate Local Gravity Gradient Matrix
+
 grav_=(mu/rstar_(1)^3)*[2 0 0 ; 0 -1 0; 0 0 -1];
 n=sqrt(mu/rstar_(1)^3);
 
-A=[3*n^2 0 0; 0 0 0; 0 0 -n^2];
-B=[0 2*n 0; -2*n 0 0; 0 0 0];
+%Indicate desired orbital time step for final rendezvous
 
-t=pi;
+t=linspace(.5,12*pi,50);
 
-s=sin(n*t*(180/pi));
-c=cos(n*t*(180/pi));
+for i=1:length(t)
+	
+	% Calculate Delta V
 
-M=[4-3*c 0 0; 6*(s-n*t) 1 0; 0 0 c];
-N=[s/n (2/n)*(1-c) 0; -(2/n)*(1-c) (4*s-3*n*t)/n 0; 0 0 s/n];
-S=[3*n*s 0 0; -6*n*(1-c) 0 0; 0 0 -n*s];
-T=[c 2*s 0; -2*s 4*c-3 0; 0 0 c];
+	s=sin(n*t(i)*(180/pi));
+	c=cos(n*t(i)*(180/pi));
 
-deltav=(T*inv(N)*M-S)*r_
+	M=[4-3*c 0 0; 6*(s-n*t) 1 0; 0 0 c];
+	N=[s/n (2/n)*(1-c) 0; -(2/n)*(1-c) (4*s-3*n*t)/n 0; 0 0 s/n];
+	S=[3*n*s 0 0; -6*n*(1-c) 0 0; 0 0 -n*s];
+	T=[c 2*s 0; -2*s 4*c-3 0; 0 0 c];
+
+	deltav_x(i)=(T*inv(N)*M-S)*r_(1);
+	deltav_y(i)=(T*inv(N)*M-S)*r_(2);
+
+end
+
+plot(t,deltav_x,t,deltav_y)
+xlabel('Time (Period Step Size)')
+ylabel('Delta V (km/s)')
+legend('Delta V: X Direction','Delta V: Y Direction')
+
+
 
 
 
