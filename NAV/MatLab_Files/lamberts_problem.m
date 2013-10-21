07a546b8d40ba1a471aed046a16061beb7d74177
tomamoline2 <tmoline@slu.edu>
2013-10-21 15:57:02 -0500

    Added initialization, need to create orbit plotting script

--- NAV/MatLab_Files/lamberts_problem.m
+++ NAV/MatLab_Files/lamberts_problem.m
@@ -3,7 +3,7 @@
 %=======================Author: Tom Moline=================================
 %=====================Approval: Nate Richard===============================
 %=====================Revision: - =========================================
-%===============Last Edit Date: October 13, 2013===========================
+%===============Last Edit Date: October 21, 2013===========================
 
 %============================Summary=======================================
 %This script serves as a preliminary method of solving lambert's problem,
@@ -13,8 +13,8 @@
 %transfer.
 
 %============================Inputs========================================
-% R1_ = Inital Displacement Vector, [x;y;z] (km)
-% R2_ = Final Displacemetn Vector, [x;y;z] (km)
+% r1_ = Inital Displacement Vector, [x;y;z] (km)
+% r2_ = Final Displacemetn Vector, [x;y;z] (km)
 % tf =Difference between time for target satellite and chasing satellite 
 %     reach point 2 (km)
 %
@@ -25,3 +25,12 @@
 % Terminal velocity vectors at each point
 
 %==========================Begin Code======================================
+
+%Initialize Constants
+mu=398600; %Earth's standard gravitational parameter,m^3/s^2
+re=6371; %Earth's radius, km
+
+%Initialize Postion Vectors and Final Time
+r1_=[re+500;0;0];
+r2_=[re+500.1;.1;0];
+
