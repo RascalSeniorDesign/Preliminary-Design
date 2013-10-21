e0939c86d24f70a736ac59f0e4b3901b456a5db5
tomamoline2 <tmoline@slu.edu>
2013-10-21 18:23:47 -0500

    Code seems to work, need to make plots of results

--- NAV/MatLab_Files/orbits_plot.m
+++ NAV/MatLab_Files/orbits_plot.m
@@ -52,12 +52,11 @@ for i=1:2
     t(i)=T(i)/1000;
 end
 
-%===========Find Magnitude of Positon and Velocity Vectors=================
 for i=1:2
-    v(i)=sqrt(v_(i,1)^2+v_(i,2)^2+v_(i,3)^2);
 %===================Find Specific Momentum Vector==========================
     tempr_=[r_(i,1) r_(i,2) r_(i,3)];
     tempv_=[v_(i,1) v_(i,2) v_(i,3)];
+    v(i)=sqrt(dot(tempv_,tempv_));
     htemp_ = cross(tempr_,tempv_);
     h(i) = sqrt(dot(htemp_,htemp_));
 %========================Find Eccentricity=================================
@@ -85,55 +84,57 @@ for i=1:2
     inc(i) = acos( htemp_(3) / h(i));
 %===============Find Longitude of Ascending Node===========================
     if( tempN_(2) < 0)
-        omega(i) = 2*pi() - acos(tempN_(1)/N(i));
+        omega(i) = 2*pi - acos(tempN_(1)/N(i));
     else
         omega(i) = acos(tempN_(1)/N(i));
     end
+
+%========================Find Mean Anomaly=================================
+    Mnot(i) = 2*atan(sqrt((1-e(i))/(1+e(i)))*tan(theta(i)/2))-(e(i)*sqrt(1-e(i)^2)*sin(theta(i)))/(1+e(i)*cos(theta(i)));
+    Enot(i) = acos((e(i)+cos(theta(i)))/(1+e(i)*cos(theta(i))));
+    E=Enot(i);
+    M=Mnot(i);
+    
+%=========================Create Perifocal Transformation Matrix===========
+    QXx_=[-sin(omega(i))*cos(inc(i))*sin(w(i))+cos(omega(i))*cos(w(i)), cos(omega(i))*cos(inc(i))*sin(w(i))+sin(omega(i))*cos(w(i)), sin(inc(i))*sin(w(i));...
+            -sin(omega(i))*cos(inc(i))*cos(w(i))-cos(omega(i))*sin(w(i)), cos(omega(i))*cos(inc(i))*cos(w(i))-sin(omega(i))*sin(w(i)), sin(inc(i))*cos(w(i));...
+            sin(omega(i))*sin(inc(i)), -cos(omega(i))*sin(inc(i)), cos(inc(i))];
+    
+%===========================Create Geocentric Transformation Matrix========
+    QxX_=[-sin(omega(i))*cos(inc(i))*sin(w(i))+cos(omega(i))*cos(w(i)), -sin(omega(i))*cos(inc(i))*cos(w(i))-cos(omega(i))*sin(w(i)), sin(omega(i))*sin(inc(i));...
+            cos(omega(i))*cos(inc(i))*sin(w(i))+sin(omega(i))*cos(w(i)), cos(omega(i))*cos(inc(i))*cos(w(i))-sin(omega(i))*sin(w(i)), -cos(omega(i))*sin(inc(i));...
+            sin(w(i))*sin(inc(i)), cos(w(i))*sin(inc(i)), cos(inc(i))];
+    
+
+    for k=1:(2*T(i))/60
+%========================Find Eccentric Anomaly============================
+        E_ratio=1;
+        M = M + sqrt(mew/a(i)^3)*(t(i));
+        if M<pi
+            E=M+e/2;
+        else
+            E=M-e/2;
+        end
+    
+        while E_ratio>10^-8
+            E_new=E-e(i)*sin(E)-M;
+            E_prime=1-e(i)*cos(E);
+            E_ratio=E_new/E_prime;
+            E=E-E_ratio;
+        end
+        thetatemp=2*atan(sqrt((1+e(i))/(1-e(i)))*tan(E(1)/2));
+        rp_=(h(i)^2/mew)*(1/(1+e(i)*cos(thetatemp)))*[cos(thetatemp);sin(thetatemp);0];
+        vp_=(mew/h(i))*[-sin(thetatemp);e(i)+cos(thetatemp);0];
+
+%==============================Define Perifocal Quantities=================
+        r_temp=QxX_*rp_;
+        v_temp=QxX_*vp_;
+    
+        rtemp_=r_temp.';
+        vtemp_=v_temp.';
+    
+        x(i,k)=rtemp_(1);
+        y(i,k)=rtemp_(2);
+        z(i,k)=rtemp_(3);
+    end
 end
-% %========================Find Mean Anomaly=================================
-% Mnot = 2*atan(sqrt((1-e)/(1+e))*tan(theta/2))-(e*sqrt(1-e^2)*sin(theta))/(1+e*cos(theta));
-% Enot = acos((e+cos(theta))/(1+e*cos(theta)));
-% E=Enot;
-% M=Mnot;
-% for k=1:5*T
-% %========================Find Eccentric Anomaly============================
-%     E_ratio=1;
-%     M = M + sqrt(mew/a^3)*(t);
-%     if M<pi
-%         E=M+e/2;
-%     else
-%         E=M-e/2;
-%     end
-%     
-%     while E_ratio>10^-8
-%         E_new=E-e*sin(E)-M;
-%         E_prime=1-e*cos(E);
-%         E_ratio=E_new/E_prime;
-%         E=E-E_ratio;
-%     end
-%     theta=2*atan(sqrt((1+e)/(1-e))*tan(E/2));
-%     rp_=(h^2/mew)*(1/(1+e*cos(theta)))*[cos(theta);sin(theta);0];
-%     vp_=(mew/h)*[-sin(theta);e+cos(theta);0];
-%     
-% %=========================Create Perifocal Transformation Matrix===========
-%     QXx_=[-sin(omega)*cos(i)*sin(w)+cos(omega)*cos(w), cos(omega)*cos(i)*sin(w)+sin(omega)*cos(w), sin(i)*sin(w);
-%         -sin(omega)*cos(i)*cos(w)-cos(omega)*sin(w), cos(omega)*cos(i)*cos(w)-sin(omega)*sin(w), sin(i)*cos(w);
-%         sin(omega)*sin(i), -cos(omega)*sin(i), cos(i)];
-%     
-% %===========================Create Geocentric Transformation Matrix========
-%     QxX_=[-sin(omega)*cos(i)*sin(w)+cos(omega)*cos(w), -sin(omega)*cos(i)*cos(w)-cos(omega)*sin(w), sin(omega)*sin(i);
-%         cos(omega)*cos(i)*sin(w)+sin(omega)*cos(w), cos(omega)*cos(i)*cos(w)-sin(omega)*sin(w), -cos(omega)*sin(i);
-%         sin(w)*sin(i), cos(w)*sin(i), cos(i)];
-%     
-% %==============================Define Perifocal Quantities=================
-%     r_temp=QxX_*rp_;
-%     v_temp=QxX_*vp_;
-%     
-%     r_=r_temp.';
-%     v_=v_temp.';
-%     
-%     x(k)=r_(1);
-%     y(k)=r_(2);
-%     z(k)=r_(3);
-%     
-% end
