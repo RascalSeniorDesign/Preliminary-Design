dddb66a36e4326ac6612a72f7f12d681f5571e24
tomamoline <tmoline@slu.edu>
2014-01-31 22:06:28 -0600

    Added comments, otherwise, no changes

--- NAV/MatLab_Files/hitEarthChecker.m
+++ NAV/MatLab_Files/hitEarthChecker.m
@@ -8,36 +8,40 @@ function [rp,collision] = hitEarthChecker(rint_,rtgt_,vtransa_,vtransb_,a)
 %==========================================================================
 % Variable Name  Variable Description      Variable Type    Variable Units
 %==========================================================================
-%      r0_       Starting positon vector      3-vector            km
-%      v0_       Starting Velocity vector     3-vector           km/s
-%      tf        Flight Time                   Scalar             min
-%      r_        Ending Position Vector       3-vector            km
-%      v_        Ending Velocity Vector       3-vector           km/s
+%      rint_     Interceptor Position vector  3-vector            km
+%      rtgt_     Target Position vector       3-vector            km
+%      vtransa_  Initial Velocity of Transfer 3-vector            km/s
+%      vtransa_  Final Velocity of Transfer   3-vector            km/s
+%      a         Transfer Orbit SM Axis        Scalar             km
 %==========================================================================
-%Initial Release, keplarSolver.m, Tom Moline, 1/31/2014
+%Initial Release, hitEarthChecker.m, Tom Moline, 1/31/2014
 
 %Begin Code
 
 %==========================================================================
 %                       Initialize Variables
 %==========================================================================
-rint_=rint_./6378.1;
+rint_=rint_./6378.1;%Convert to canonical units,DU
 rtgt_=rtgt_./6378.1;
-vtransa_=vtransa_./7.9053838;
+vtransa_=vtransa_./7.9053838; %Convert to canonical units, DU/TU
 vtransb_=vtransb_./7.9053838;
 a=a/6378.1;
 
-if dot(rint_,vtransa_)<0.0 && dot(rtgt_,vtransb_)>0.0
+%==========================================================================
+%     Check flight path angles at transfer points for potential issues
+%==========================================================================
+
+if dot(rint_,vtransa_)<0.0 && dot(rtgt_,vtransb_)>0.0 
     zi=-1/(2*a);
     ht_=cross(rint_,vtransa_);
     ht=sqrt(sum(abs(ht_)).^2);
     p=ht^2;
     e=sqrt((a-p)/a);
-    rp=a*(1-e);
+    rp=a*(1-e); %Find perigee radius, if necessary
     if rp<1
-        collision=-1;
+        collision=-1; %Collision occured
     else
-        collision=1;
+        collision=1; %Collision did not occur
     end
 end
         
